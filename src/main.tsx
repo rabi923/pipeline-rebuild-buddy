1ed7b5cb171ef5d875dac2558c9e38f2f8de3781
rabi923 <146437715+rabi923@users.noreply.github.com>
2025-10-18 23:40:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
+import React from 'react';
 import App from "./App.tsx";
 import "./index.css";
 import 'leaflet/dist/leaflet.css'; 
-
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 createRoot(document.getElementById("root")!).render(<App />);
